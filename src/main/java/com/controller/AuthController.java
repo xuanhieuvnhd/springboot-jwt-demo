9164017a27302bc4baae029a287938a0c34118c2
xuanhieuvnhd <xuanhieuvn@gmail.com>
2022-11-07 17:08:13 +0700

    update

--- src/main/java/com/controller/AuthController.java
+++ src/main/java/com/controller/AuthController.java
@@ -17,7 +17,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
+//@RequestMapping("/api")
+@CrossOrigin("*")
 public class AuthController {
     @Autowired
     private AuthenticationManager authenticationManager;
